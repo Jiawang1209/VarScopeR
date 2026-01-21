72c479673ef080e0a699c2fd18574c2351e46304
Jiawang1209 <Jiawang1209@163.com>
2026-01-21 23:45:39 +0800

    update

--- R/FindVariableFeatures.R
+++ R/FindVariableFeatures.R
@@ -1,4 +1,42 @@
-FindVariableFeatures <- function(mat, group){
+#' Identify variable features by variance decomposition in VarScope object
+#'
+#' Identify perturbation-driven variable features by decomposing feature-wise
+#' variance into within-group and between-group components. Features showing
+#' low within-group variability and high between-group variability are retained
+#' as variable features.
+#' @param object A \code{VarScope} object
+#' containing normalized feature abundance data .
+#' @param assay Character.
+#' Name of the assay to use for variable feature detection.
+#' @param var_within_cutoff Numeric.
+#' the quantile threshold for within-group variance.
+#' @param var_between_cutoff Numeric.
+#' Lower threshold for between-group variance.
+#' @param seed Integer (default = 1115).
+#' Random seed for reproducibility.
+#'
+#' @returns A \code{VarScope} object containing the variable feature data,
+#' @export
+#'
+#' @examples NULL
+FindVariableFeatures <- function(object,
+                                 assay = "norm",
+                                 var_within_cutoff = 0.4,
+                                 var_between_cutoff = 0.5,
+                                 seed = 1115
+                                 ){
+
+  set.seed(seed)
+
+  # check object
+  stopifnot(inherits(object, "VarScope"))
+
+  # get data
+  mat <- object$assays[[assay]]
+
+  # based normal
+  group <- object$meta$sample
+
 
   # check input
   stopifnot(ncol(mat) == nrow(group))
@@ -67,5 +105,16 @@ FindVariableFeatures <- function(mat, group){
     check.names = FALSE
   )
 
+  out2 <- out %>%
+    dplyr::filter(var_within <= quantile(out$var_within, var_within_cutoff)) %>%
+    dplyr::filter(eta2_between >= var_between_cutoff) %>%
+    dplyr::arrange(desc(eta2_between))
+
+
+  # out object
+  object$features$stats <- out
+  object$features$stats_filter <- out2
+
+  return(object)
 
 }
--- R/NormalizeData.R
+++ R/NormalizeData.R
@@ -1,3 +1,73 @@
-NormalizeData <- function(){
+#' Normalize feature abundance data in VarScope object
+#'
+#' Normalize raw feature abundance data (e.g. OTU/ASV tables) stored in a
+#' \code{VarScope} object. The normalized data will be saved as a new assay
+#' within the object, while the original data remain unchanged.
+#' @param object A \code{VarScope} object
+#' containing raw feature abundance data .
+#' @param assay Character.
+#' Name of the input assay to be normalized
+#' @param new.assay Character.
+#' Name of the normalized data
+#' @param normalization.method Character.
+#' Data transformation methods applied before correlation analysis.
+#' Options include:
+#' "none" (raw data),
+#' "scale" (z-score standardization),
+#' "center" (mean centering only),
+#' "log2" (log2 transfrom),
+#' "log10" (log10 transfrom),
+#' "ln" (natural transfrom ),
+#' "rrarefy" (random rarefaction using \code{vegan::rrarefy}),
+#' "rrarefy_relative" (rarefy then convert to relative abundance).
+#' @param seed Integer (default = 1115).
+#' Random seed for reproducibility.
+#'
+#' @returns A \code{VarScope} object containing the normalized data,
+#' @export
+#'
+#' @examples NULL
+NormalizeData <- function(object,
+                          assay = "raw",
+                          new.assay = "norm",
+                          normalization.method = c("none", "log2", "log10", "ln", "rrarefy", "rrarefy_relative", "clr"),
+                          seed = 1115
+                          ){
+
+  set.seed(seed)
+
+  # check object
+  stopifnot(inherits(object, "VarScope"))
+
+  # argument check
+  normalization.method <-  match.arg(normalization.method)
+
+  # get matrix
+  mat <- object$assays[[assay]]
+
+  # data transfrom
+  mat <- switch (
+    normalization.method,
+    none = mat,
+    log2 = log2(mat + 1),
+    log10 = log10(mat + 1),
+    ln = log(mat + 1),
+    rrarefy = t(vegan::rrarefy(t(mat), min(colSums(mat)))),
+    rrarefy_relative = t(vegan::rrarefy(t(mat), min(colSums(mat)))) / colSums(t(vegan::rrarefy(t(mat), min(colSums(mat))))),
+    clr = sweep(log(mat + 1e-6), 2, colMeans(log(mat + 1e-6)), "-")
+  )
+
+  # out
+  object$assays[[new.assay]] <- mat
+
+  # params
+  object$params$NormalizeData <- list(
+    assay = assay,
+    new.assay = new.assay,
+    method = normalization.method
+  )
+
+  # output
+  return(object)
 
 }
